6eff2defd2e239ea2b1cb3293e06d4a97704e962
Julio_Rosa <juliorosa.ibt@gmail.com>
2025-04-08 14:52:58 -0300

    first commit~

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:false,
+    nav:true,
+    mouseDrag:false,
+    autoplay:false,
+    animateOut: 'slideOuRight',
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+function showMenu(){
+    let menu = document.querySelector(".mobile-menu");
+    if(menu.classList.contains('open')){
+        menu.classList.remove('open');
+        document.querySelector(".icon").src = "/src/images/menu_white_36dp.svg";
+
+    }else{
+        menu.classList.add('open');
+        document.querySelector(".icon").src = "/src/images/close_white_36dp.svg";
+    }
+}
